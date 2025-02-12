d27479695ed3b5f8bbbd2e6299f28679f8122e72
vtbprog <vtb.prog@gmail.com>
2025-02-11 18:07:53 -0600

    Solution for 02_basic_calculator/07_for
    Signed-off-by: vtbprog <vtb.prog@gmail.com>

---
+++ solutions/02_basic_calculator/07_for/src/lib.rs
@@ -0,0 +1,39 @@
+// Rewrite the factorial function using a `for` loop.
+pub fn factorial(n: u32) -> u32 {
+    let mut prod: u32 = 1;
+
+    if n == 0 {
+        return 1;
+    }
+
+    for i in 1..(n + 1) {
+        prod *= i;
+    }
+
+    prod
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::factorial;
+
+    #[test]
+    fn first() {
+        assert_eq!(factorial(0), 1);
+    }
+
+    #[test]
+    fn second() {
+        assert_eq!(factorial(1), 1);
+    }
+
+    #[test]
+    fn third() {
+        assert_eq!(factorial(2), 2);
+    }
+
+    #[test]
+    fn fifth() {
+        assert_eq!(factorial(5), 120);
+    }
+}
